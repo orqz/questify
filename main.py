f8dd25cdceea9736b0a9e463ff4882a5c6a4eacb
orqz <hac3wow@gmail.com>
2026-02-06 11:11:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import os
+import sys
+import shutil
+import time
 import requests
 import webbrowser
-import sys
-from rich import print
-import time
 
 
-def getreq():
-    os.system('pip install colorama')
-    os.system('pip install rich')
-    os.system('clear')
 
-def questify():
-    url = "https://raw.githubusercontent.com/orqz/questify/refs/heads/main/games.json"
-    data = requests.get(url).json()
+def quest():
+
+    try:
+        import requests
+    except ImportError:
+        os.system('pip install requests')
+
+    os.system('cls')
+    
     while True:
+        url = "https://raw.githubusercontent.com/orqz/questify/refs/heads/main/games.json"
+        data = requests.get(url).json()
         gameselected = input("please selecte ur game : ").strip().lower()
         gamedata = data.get(gameselected)
         if gamedata == None:
@@ -23,20 +27,49 @@ def questify():
         else:
             game_exe = gamedata['exe']
             game_folder = gamedata['folder']
-            currentfile = os.path.abspath(sys.argv[0])
+            src = "questify.exe"
+            dst = os.path.join(game_folder, "questify.exe")
             os.mkdir(game_folder)
-            os.rename(currentfile, os.path.join(game_folder, game_exe))
-            timer()
+            shutil.copy(src, dst)
+            new_name = os.path.join(game_folder, game_exe + ".exe")
+            os.rename(dst, new_name)
+            print(f"{game_folder} Folder has been Created :) open the {game_exe}.exe inside it and enjoy")
+            time.sleep(15)
 
-   
+def timer():
+    total_seconds = 15 * 60 + 30  
+
+    while total_seconds > 0:
+        minutes, seconds = divmod(total_seconds, 60)
+        print(f"\rTime left: {minutes:02d}:{seconds:02d}", end="", flush=True)
+        time.sleep(1)
+        total_seconds -= 1
+
+    print("\rTime left: 00:00")
+    sys.exit(0) 
+
 def showlist():
     webbrowser.open_new_tab('https://github.com/orqz/questify/blob/main/games.json')
-
-def die():
-    os.system('exit')
     
-def timer():
-    time.sleep(15)    
+def die():
+   sys.exit(0) 
+    
+def menu():
+    print(banner)
+    print(" [1] Start Questify : ") 
+    print(" [2] Game List ")
+    print(" [3] Exit ")
+
+    i = input()
+    
+    match i:
+        case "1":
+            quest()
+        case "2":
+            showlist()
+        case "3":
+            die()
+        
 
 
 
@@ -54,36 +87,8 @@ banner = r"""
                      >> github.com/orqz <<
 """
 
-def rename():
-    currentfile = os.path.abspath(sys.argv[0])
-    os.rename(currentfile, 'questify.exe')
-    os.system("cls")
 
-def menu():
-    print(banner)
-    print(" [1] Start Questify :) ")
-    print(" [2] Game List ")
-    print(" [3] Exit ")
-
-    i = input()
-    
-    match i:
-        case "1":
-            questify()
-        case "2":
-            showlist()
-            os.system("cls")
-            menu()
-        case "3":
-            die()
-
-
-#if not "questify.exe" in os.path.abspath(sys.argv[0]):
-    #rename()
-
-
-menu()
-
-
-
-
+if os.path.exists('questify.exe'):
+    menu()
+else:
+    timer()
